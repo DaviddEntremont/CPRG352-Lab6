c71e2e39b209981a899fd5ab4c5dd3681788c07b
David <David@DESKTOP-0SIBBB5.hitronhub.home>
2021-10-24 17:00:02 -0600

    Deeper into coding out the shopinglist and shoppinglistservlet Playing with the array list functionality to retrieve values in the JSP.

--- src/java/servlets/ShoppingListServlet.java
+++ src/java/servlets/ShoppingListServlet.java
@@ -1,10 +1,12 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 public class ShoppingListServlet extends HttpServlet {
@@ -13,14 +15,37 @@ public class ShoppingListServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        HttpSession sessionObject = request.getSession();
+        
         String username = request.getParameter("username");
+        
+        String logout = request.getParameter("action");
+        
+        if (logout != null && logout.equals("logout")) {
+            
+            sessionObject.invalidate();
+            
+            sessionObject = request.getSession();
+            
+            sessionObject.setAttribute("loggedout", true);
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+            
+            return;
+        }
+        
         if (username != null && !username.isEmpty()) {
+            
             // give the user info on one user
              getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+             
+             return;
             
         } else {
             // return all users
             getServletContext().getRequestDispatcher("/WEB-INF/shoppinglist.jsp").forward(request, response);
+            
+            return;
 
     }
     }
@@ -30,6 +55,37 @@ public class ShoppingListServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        HttpSession sessionObject = request.getSession();
+        
+        ArrayList shoppingListArray = new ArrayList<>();
+        
+        String action = request.getParameter("action");
+        
+        String newItem = request.getParameter("item");
+        
+        if (action == null || action.equals("")) {
+            
+        }
+        
+        else if (action.equals("add")) {
+            
+            shoppingListArray.add(newItem);
+            
+            sessionObject.setAttribute("shoppingListItems", shoppingListArray);
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppinglist.jsp").forward(request, response);
+            
+            return;
+            
+        }
+        else if (action.equals("delete")) {
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppinglist.jsp").forward(request, response);
+            
+            return;
+            
+        }
        
     }
 
--- web/WEB-INF/shoppinglist.jsp
+++ web/WEB-INF/shoppinglist.jsp
@@ -7,10 +7,30 @@
         <title>Shopping List</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
-        <c:if test="${nameTest}">
+        <h1>Shopping List</h1>
         <p>Hello ${username}</p>
         <p><a href="<c:url value='/ShoppingList'><c:param name='action' value='logout' /></c:url>">Logout</a></p>
-        </c:if>
+
+        <form action="" method="POST">
+            <h2>Add Item</h2>
+            <input type="text" name="item"><input type="submit" value="Add Item">
+            <input type="hidden" name="action" value="add">
+        </form>
+        
+        <form action="" method="post">
+            <ul>
+                <c:forEach items="${shoppingListItems}" var="account">
+                
+                        <li><input type="radio" name="item" value="${shoppingListItems.get()}">${shoppingListItems.get()}</li>
+                    
+            </c:forEach>
+            </ul>
+        
+            <input type="submit" value="Delete">
+            <input type="hidden" name="action" value="delete">
+            </form>
+            
+        
+        
     </body>
 </html>
